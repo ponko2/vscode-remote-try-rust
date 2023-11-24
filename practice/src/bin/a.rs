2c9d28bcbbbfbfccefe8eb61b0c9867a05f5f3f5
Takahito Nakano <kano@ponko2.jp>
2023-11-24 22:30:25 +0900

    Submit practice A

--- practice/src/bin/a.rs
+++ practice/src/bin/a.rs
@@ -3,6 +3,10 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        (b, c): (usize, usize),
+        s: String
     }
+
+    println!("{} {}", a + b + c, s)
 }
