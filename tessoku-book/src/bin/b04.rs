2f7a408476c5c09f8c0d564512724180154bced6
Takahito Nakano <kano@ponko2.jp>
2023-11-26 20:18:29 +0900

    Submit tessoku-book B04

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,6 +1,16 @@
+use proconio::marker::Chars;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: Chars,
+    }
+
+    let size = n.len();
+    println!(
+        "{}",
+        (0..size).fold(0, |acc, i| acc
+            + if n[i] == '1' { 1 << (size - i - 1) } else { 0 })
+    );
 }
