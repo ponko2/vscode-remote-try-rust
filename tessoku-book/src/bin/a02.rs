52382404d1ddf107b9f3bb298b3ac65cd3c25706
Takahito Nakano <kano@ponko2.jp>
2023-11-26 17:39:46 +0900

    Submit tessoku-book A02

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -2,5 +2,14 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, x): (isize, i32),
+        a: [i32; n],
+    }
+
+    if a.iter().any(|&i| i == x) {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
