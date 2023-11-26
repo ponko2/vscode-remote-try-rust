bde265f70a3f04080686cb55737c16a39ef96e68
Takahito Nakano <kano@ponko2.jp>
2023-11-26 19:20:55 +0900

    Submit tessoku-book A04

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -2,5 +2,13 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    for i in (0..=9).rev() {
+        print!("{}", (n / (1 << i)) % 2);
+    }
+
+    println!();
 }
