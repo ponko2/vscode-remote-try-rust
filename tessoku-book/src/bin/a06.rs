1bf767c34e1d0bd98296520992e4e21d5c4b09a0
Takahito Nakano <kano@ponko2.jp>
2023-11-27 00:18:20 +0900

    Submit tessoku-book A06

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, q): (usize, usize),
+        a: [usize; n],
+        lr: [(usize, usize); q],
+    }
+
+    let s: Vec<_> = std::iter::once(0)
+        .chain(a.iter().scan(0, |state, a| {
+            *state += a;
+            Some(*state)
+        }))
+        .collect();
+
+    for (l, r) in lr {
+        println!("{}", s[r] - s[l - 1]);
+    }
 }
