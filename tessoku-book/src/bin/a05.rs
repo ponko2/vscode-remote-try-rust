a0a07382808a5ba7d66a5b913c97d776e1f721b6
Takahito Nakano <kano@ponko2.jp>
2023-11-26 21:07:23 +0900

    Submit tessoku-book A05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, k): (usize, usize),
+    }
+
+    let mut answer = 0;
+    for x in 1..=n {
+        for y in 1..=n {
+            if let Some(z) = k.checked_sub(x + y) {
+                if z >= 1 && z <= n {
+                    answer += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", answer);
 }
