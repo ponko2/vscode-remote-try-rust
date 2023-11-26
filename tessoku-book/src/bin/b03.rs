208d0a2ade54c3e4f1163cb1345eb640a7f0479a
Takahito Nakano <kano@ponko2.jp>
2023-11-26 18:31:30 +0900

    Submit tessoku-book B03

--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    for i in 0..(n - 2) {
+        for j in (i + 1)..(n - 1) {
+            for k in (j + 1)..n {
+                if a[i] + a[j] + a[k] == 1000 {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("No");
 }
