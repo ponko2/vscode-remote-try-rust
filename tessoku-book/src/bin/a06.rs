50ff5f9ff1d68d3349b525661b981519626481da
Takahito Nakano <kano@ponko2.jp>
2023-11-27 00:18:20 +0900

    Submit tessoku-book A06

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, q): (usize, usize),
+        a: [usize; n],
+        lr: [(usize, usize); q],
+    }
+
+    let mut s = Vec::with_capacity(n + 1);
+    s.push(0);
+    for i in 0..n {
+        s.push(s[i] + a[i]);
+    }
+
+    for i in 0..q {
+        let (l, r) = lr[i];
+        println!("{}", s[r] - s[l - 1]);
+    }
 }
