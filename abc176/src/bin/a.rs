77b4e7ed2cc89f3e33d2bc8b9fad0146e14a4e3c
Takahito Nakano <kano@ponko2.jp>
2023-11-30 15:34:26 +0000

    Submit abc176 A

--- abc176/src/bin/a.rs
+++ abc176/src/bin/a.rs
@@ -1,6 +1,12 @@
+#![allow(unstable_name_collisions)]
+use num_integer::Integer;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, x, t): (usize, usize, usize),
+    }
+
+    println!("{}", n.div_ceil(&x) * t);
 }
