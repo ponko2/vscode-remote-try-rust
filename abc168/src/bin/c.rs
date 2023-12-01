446feaf3c8543ce66672a27f89c11635c4063588
Takahito Nakano <kano@ponko2.jp>
2023-12-01 19:01:03 +0000

    Submit abc168 C

--- abc168/src/bin/c.rs
+++ abc168/src/bin/c.rs
@@ -2,5 +2,12 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, h, m): (f64, f64, f64, f64),
+    }
+
+    println!(
+        "{}",
+        (a * a + b * b - 2. * a * b * (h * 30. - m * 5.5).to_radians().cos()).sqrt()
+    );
 }
