0d59be699d3326e3690396a1aa7793ea167c718e
Takahito Nakano <kano@ponko2.jp>
2023-12-02 19:39:47 +0000

    Submit abc175 B

--- abc175/src/bin/b.rs
+++ abc175/src/bin/b.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut l: [usize; n],
+    }
+
+    l.sort();
+
+    let mut answer = 0;
+    for i in 0..n {
+        for j in (i + 1)..n {
+            for k in (j + 1)..n {
+                if l[i] != l[j] && l[j] != l[k] && l[i] + l[j] > l[k] {
+                    answer += 1;
+                }
+            }
+        }
+    }
+
+    println!("{answer}");
 }
