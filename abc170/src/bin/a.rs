b347efced192f25774d2aef241906a0190b8a428
Takahito Nakano <kano@ponko2.jp>
2023-12-02 12:28:52 +0000

    Submit abc170 A

--- abc170/src/bin/a.rs
+++ abc170/src/bin/a.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (x1, x2, x3, x4): (usize, usize, usize, usize),
+    }
+
+    println!(
+        "{}",
+        if x1 == 0 {
+            1
+        } else if x2 == 0 {
+            2
+        } else if x3 == 0 {
+            3
+        } else if x4 == 0 {
+            4
+        } else {
+            5
+        }
+    )
 }
