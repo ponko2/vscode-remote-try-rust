5a2b0b4a4b9110e96e3338dc0eac82ab198fa462
Takahito Nakano <kano@ponko2.jp>
2023-12-02 16:23:50 +0000

    Submit abc174 B

--- abc174/src/bin/b.rs
+++ abc174/src/bin/b.rs
@@ -2,5 +2,10 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, d): (usize, f64),
+        v: [(f64, f64); n],
+    }
+
+    println!("{}", v.iter().filter(|&&(x, y)| x.hypot(y) <= d).count());
 }
