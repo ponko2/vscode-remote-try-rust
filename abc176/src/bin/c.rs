296cbd281349fb5d85faab4d6e5f6b09799afbfb
Takahito Nakano <kano@ponko2.jp>
2023-12-02 15:26:18 +0000

    Submit abc176 C

--- abc176/src/bin/c.rs
+++ abc176/src/bin/c.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    println!(
+        "{}",
+        a.iter()
+            .scan(0, |max, &i| {
+                *max = i.max(*max);
+                Some(*max - i)
+            })
+            .sum::<usize>()
+    );
 }
