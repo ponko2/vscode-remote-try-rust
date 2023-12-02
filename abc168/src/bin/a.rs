cc22fb5826e4d552843517a31e7ac5ee007273bb
Takahito Nakano <kano@ponko2.jp>
2023-12-02 19:55:08 +0000

    Submit abc168 A

--- abc168/src/bin/a.rs
+++ abc168/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    println!(
+        "{}",
+        match n % 10 {
+            2 | 4 | 5 | 7 | 9 => "hon",
+            0 | 1 | 6 | 8 => "pon",
+            3 => "bon",
+            _ => unreachable!(),
+        }
+    )
 }
