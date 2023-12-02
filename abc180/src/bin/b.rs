cfcb09d719b3a0608bd2f2a254270cc99553ec5a
Takahito Nakano <kano@ponko2.jp>
2023-12-02 16:00:42 +0000

    Submit abc180 B

--- abc180/src/bin/b.rs
+++ abc180/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: [i64; n],
+    }
+
+    println!("{}", x.iter().map(|i| i.abs()).sum::<i64>());
+    println!(
+        "{}",
+        x.iter().map(|i| i.abs().pow(2) as f64).sum::<f64>().sqrt()
+    );
+    println!("{}", x.iter().map(|i| i.abs()).max().unwrap());
 }
