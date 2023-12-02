0a328cd7dca27edc1d0dd4836e57d89e223191c6
Takahito Nakano <kano@ponko2.jp>
2023-12-02 14:51:38 +0000

    Submit abc181 B

--- abc181/src/bin/b.rs
+++ abc181/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        v: [(usize, usize); n],
+    }
+
+    println!(
+        "{}",
+        v.iter()
+            .map(|(a, b)| (a + b) * (b - a + 1) / 2)
+            .sum::<usize>()
+    );
 }
