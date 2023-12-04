0d69aafca5051d6b24febc120abefee632fce0f8
Takahito Nakano <kano@ponko2.jp>
2023-12-04 13:12:11 +0000

    Submit abc171 A

--- abc171/src/bin/a.rs
+++ abc171/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: char,
+    }
+
+    println!(
+        "{}",
+        match a {
+            'A'..='Z' => 'A',
+            'a'..='z' => 'a',
+            _ => unreachable!(),
+        }
+    )
 }
