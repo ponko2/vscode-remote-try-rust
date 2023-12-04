118db2e6accd1b7368d67fa27caa93f12fee8122
Takahito Nakano <kano@ponko2.jp>
2023-12-04 12:41:11 +0000

    Submit abc175 A

--- abc175/src/bin/a.rs
+++ abc175/src/bin/a.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    let mut count = 0;
+    let mut answer = 0;
+    for c in s.chars() {
+        match c {
+            'R' => count += 1,
+            'S' => count = 0,
+            _ => unreachable!(),
+        }
+        answer = answer.max(count);
+    }
+
+    println!("{answer}");
 }
