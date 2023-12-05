7c632982d7c99b7ff50019d6bc0690dd302eadf0
Takahito Nakano <kano@ponko2.jp>
2023-12-05 13:40:02 +0000

    Submit abc184 B

--- abc184/src/bin/b.rs
+++ abc184/src/bin/b.rs
@@ -1,6 +1,21 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (_, x): (usize, usize),
+        s: Chars,
+    }
+
+    let mut answer = x;
+
+    for v in s {
+        match v {
+            'x' => answer = answer.saturating_sub(1),
+            'o' => answer += 1,
+            _ => unreachable!(),
+        }
+    }
+
+    println!("{answer}");
 }
