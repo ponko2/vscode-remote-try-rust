46e2ec4435923a5555696389bc97931524c37f35
Takahito Nakano <kano@ponko2.jp>
2023-12-05 13:55:12 +0000

    Submit abc176 B

--- abc176/src/bin/b.rs
+++ abc176/src/bin/b.rs
@@ -1,6 +1,17 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Bytes};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: Bytes,
+    }
+
+    println!(
+        "{}",
+        if n.iter().fold(0, |sum, c| sum + (c - b'0') as usize) % 9 == 0 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
