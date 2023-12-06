452f0e75f207e3b661c7f43bbcaa2be71801b648
Takahito Nakano <kano@ponko2.jp>
2023-12-06 22:17:52 +0900

    Submit abc192 B

--- abc192/src/bin/b.rs
+++ abc192/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    if s.chars()
+        .enumerate()
+        .any(|(i, c)| (i % 2 == 0) ^ c.is_ascii_lowercase())
+    {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }
