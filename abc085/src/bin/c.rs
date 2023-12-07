63a9d908d54776ac2a25778210bb4440d49bedbe
Takahito Nakano <kano@ponko2.jp>
2023-12-07 13:06:55 +0000

    Submit abc085 C

--- abc085/src/bin/c.rs
+++ abc085/src/bin/c.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, y): (usize, usize),
+    }
+
+    for i in 0..=n {
+        for j in 0..=n {
+            if let Some(k) = n.checked_sub(i + j) {
+                if i * 10000 + j * 5000 + k * 1000 == y {
+                    println!("{i} {j} {k}");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
