7fe3639bc85d12e1608219bdcc37ef3da5a3adf4
Takahito Nakano <kano@ponko2.jp>
2023-12-08 13:07:30 +0000

    Submit abc044 A

--- abc044/src/bin/a.rs
+++ abc044/src/bin/a.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        x: usize,
+        y: usize,
+    }
+
+    let mut answer = 0;
+
+    for i in 1..=n {
+        if i <= k {
+            answer += x;
+        } else {
+            answer += y;
+        }
+    }
+
+    println!("{answer}");
 }
