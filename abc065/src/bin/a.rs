37ac308974db67cdea86ba7b0dfeace0bda92111
Takahito Nakano <kano@ponko2.jp>
2023-12-10 14:22:51 +0000

    Submit abc065 A

--- abc065/src/bin/a.rs
+++ abc065/src/bin/a.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (x, a, b): (i64, i64, i64),
+    }
+
+    let y = b - a;
+
+    println!(
+        "{}",
+        if y <= 0 {
+            "delicious"
+        } else if y > x {
+            "dangerous"
+        } else {
+            "safe"
+        }
+    );
 }
