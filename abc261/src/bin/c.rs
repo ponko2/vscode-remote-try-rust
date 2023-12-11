68e339b0d74e56a84197ef0dd1bdd0621f7cffcf
Takahito Nakano <kano@ponko2.jp>
2023-12-11 13:40:53 +0000

    Submit abc261 C

--- abc261/src/bin/c.rs
+++ abc261/src/bin/c.rs
@@ -1,6 +1,23 @@
+use std::collections::HashMap;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [String;n],
+    }
+
+    let mut h = HashMap::new();
+
+    for si in &s {
+        let c = h.entry(si).or_insert(0);
+        if *c == 0 {
+            println!("{si}");
+        } else {
+            println!("{si}({})", *c);
+        }
+        *c += 1;
+    }
 }
