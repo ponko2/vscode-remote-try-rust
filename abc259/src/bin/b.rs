e1b5525a35882fff9217546ea9ef9bde5903f0bf
Takahito Nakano <kano@ponko2.jp>
2023-12-12 11:32:41 +0000

    Submit abc259 B

--- abc259/src/bin/b.rs
+++ abc259/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, d): (f64, f64, f64),
+    }
+
+    let d = d.to_radians();
+
+    println!(
+        "{} {}",
+        a * d.cos() - b * d.sin(),
+        a * d.sin() + b * d.cos()
+    );
 }
