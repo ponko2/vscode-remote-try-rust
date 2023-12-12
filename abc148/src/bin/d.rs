fd4266b9977ddf001bd97e6b5ff60b42c4cefaa9
Takahito Nakano <kano@ponko2.jp>
2023-12-12 11:56:14 +0000

    Submit abc148 D

--- abc148/src/bin/d.rs
+++ abc148/src/bin/d.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize;n],
+    }
+
+    let mut ans = 0;
+    let mut i = 1;
+
+    for a in &a {
+        if *a != i {
+            ans += 1;
+        } else {
+            i += 1;
+        }
+    }
+
+    if ans == a.len() {
+        println!("-1");
+    } else {
+        println!("{ans}");
+    }
 }
