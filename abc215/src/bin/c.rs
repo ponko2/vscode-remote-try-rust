39f55f53079a0b74a3174659d599d6fb3822a63f
Takahito Nakano <kano@ponko2.jp>
2023-12-12 12:36:13 +0000

    Submit abc215 C

--- abc215/src/bin/c.rs
+++ abc215/src/bin/c.rs
@@ -1,6 +1,21 @@
-use proconio::{fastout, input};
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (s, k): (Chars, usize),
+    }
+
+    println!(
+        "{}",
+        s.iter()
+            .sorted()
+            .permutations(s.len())
+            .unique()
+            .nth(k - 1)
+            .unwrap()
+            .into_iter()
+            .collect::<String>()
+    );
 }
