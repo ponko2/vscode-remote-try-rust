213b6ef0c68caed5c75fc1e682baafe66cfdd39c
Takahito Nakano <kano@ponko2.jp>
2023-12-14 11:31:40 +0000

    Submit abc109 B

--- abc109/src/bin/b.rs
+++ abc109/src/bin/b.rs
@@ -1,6 +1,23 @@
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        w: [String;n],
+    }
+
+    let mut l = w[0].chars().last().unwrap();
+    let mut c = HashSet::from([&w[0]]);
+    for w in &w[1..] {
+        if c.contains(w) || !w.starts_with(l) {
+            println!("No");
+            return;
+        }
+        c.insert(w);
+        l = w.chars().last().unwrap();
+    }
+
+    println!("Yes");
 }
