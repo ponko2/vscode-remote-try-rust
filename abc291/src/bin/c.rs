539792991144b2bb9e7c76b3eff2d644904d4d5a
Takahito Nakano <kano@ponko2.jp>
2023-12-16 11:04:39 +0000

    Submit abc291 C

--- abc291/src/bin/c.rs
+++ abc291/src/bin/c.rs
@@ -1,6 +1,30 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        s: Chars,
+    }
+
+    let mut p = (0, 0);
+    let mut set = HashSet::new();
+    set.insert(p);
+
+    for si in &s {
+        match *si {
+            'R' => p.0 += 1,
+            'L' => p.0 -= 1,
+            'U' => p.1 += 1,
+            'D' => p.1 -= 1,
+            _ => unreachable!(),
+        }
+        if !set.insert(p) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
