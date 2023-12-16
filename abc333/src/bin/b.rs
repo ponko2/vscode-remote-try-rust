bceb86ff2a41d66e3b477fb8fcc87e77898afca8
Takahito Nakano <kano@ponko2.jp>
2023-12-16 14:05:30 +0000

    Submit abc333 B

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -1,6 +1,21 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let s = (s[0] as i16).abs_diff(s[1] as i16);
+    let t = (t[0] as i16).abs_diff(t[1] as i16);
+
+    println!(
+        "{}",
+        if s.min(5 - s) == t.min(5 - t) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
