1fb8854b3d7b207d908467cc32c82f2f8e217ab5
Takahito Nakano <kano@ponko2.jp>
2023-12-17 14:49:45 +0000

    Submit abc103 A

--- abc103/src/bin/a.rs
+++ abc103/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        mut a: [isize;3],
+    }
+
+    a.sort();
+
+    let mut ans = 0;
+
+    for i in 0..2 {
+        ans += (a[i + 1] - a[i]).abs();
+    }
+
+    println!("{ans}");
 }
