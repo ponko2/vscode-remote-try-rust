3856e5fd3ace75d2c7682e00b1cd8b229e3b022f
Takahito Nakano <kano@ponko2.jp>
2023-12-18 11:19:37 +0000

    Submit abc226 B

--- abc226/src/bin/b.rs
+++ abc226/src/bin/b.rs
@@ -1,6 +1,21 @@
+use std::collections::HashSet;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    let mut s = HashSet::new();
+    for _ in 0..n {
+        input! {
+            l: usize,
+            a: [usize;l],
+        }
+        s.insert(a);
+    }
+
+    println!("{}", s.len());
 }
