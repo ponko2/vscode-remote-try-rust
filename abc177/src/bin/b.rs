3b290ad758f963a8d2860b36c907b33ca27796cc
Takahito Nakano <kano@ponko2.jp>
2023-12-20 11:51:53 +0000

    Submit abc177 B

--- abc177/src/bin/b.rs
+++ abc177/src/bin/b.rs
@@ -1,6 +1,22 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let mut ans = t.len();
+    for i in 0..=(s.len() - t.len()) {
+        let mut diff = 0;
+        for j in 0..t.len() {
+            if s[i + j] != t[j] {
+                diff += 1;
+            }
+        }
+        ans = ans.min(diff);
+    }
+
+    println!("{ans}");
 }
