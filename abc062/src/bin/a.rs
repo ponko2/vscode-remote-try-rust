b4c9f5b1f69bc7cfaf69b7898fc0b8d7f3c20854
Takahito Nakano <kano@ponko2.jp>
2023-12-21 12:51:35 +0000

    Submit abc062 A

--- abc062/src/bin/a.rs
+++ abc062/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (x, y): (usize, usize),
+    }
+
+    for g in [vec![1, 3, 5, 7, 8, 10, 12], vec![4, 6, 9, 11], vec![2]] {
+        if g.contains(&x) && g.contains(&y) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
