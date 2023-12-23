aeca99b8e278bd4ce098fa5c93ae29cc8f70b5ab
Takahito Nakano <kano@ponko2.jp>
2023-12-23 09:41:58 +0000

    Submit abc109 A

--- abc109/src/bin/a.rs
+++ abc109/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    for c in 1..=3 {
+        if (a * b * c) % 2 == 1 {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
