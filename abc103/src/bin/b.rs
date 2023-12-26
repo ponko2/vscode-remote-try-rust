f66c1abb9ae29f963fbc02fe125720c740ad28c9
Takahito Nakano <kano@ponko2.jp>
2023-12-26 11:17:23 +0000

    Submit abc103 B

--- abc103/src/bin/b.rs
+++ abc103/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        mut s: String,
+        t: String,
+    }
+
+    for _ in 0..s.len() {
+        if t == s {
+            println!("Yes");
+            return;
+        }
+        let c = s.pop().unwrap();
+        s.insert(0, c);
+    }
+
+    println!("No");
 }
