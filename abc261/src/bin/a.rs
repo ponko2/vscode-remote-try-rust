4813267e3d2bd2b3db7ad1429abba9db151e0315
Takahito Nakano <kano@ponko2.jp>
2024-01-03 21:36:42 +0900

    Submit abc261 A

--- abc261/src/bin/a.rs
+++ abc261/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (l1, r1, l2, r2): (usize, usize, usize, usize),
+    }
+
+    println!("{}", r1.min(r2).saturating_sub(l1.max(l2)));
 }
