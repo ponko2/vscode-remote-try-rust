c401f4dcc2f8c08206ba219eeb11e149edcd07c7
Takahito Nakano <kano@ponko2.jp>
2024-01-04 19:04:02 +0900

    Submit abc291 A

--- abc291/src/bin/a.rs
+++ abc291/src/bin/a.rs
@@ -1,6 +1,13 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    println!(
+        "{}",
+        s.iter().position(|&c| c.is_ascii_uppercase()).unwrap() + 1
+    );
 }
