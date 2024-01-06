661991f0250665b49f4520552d99acc5ebf9154f
Takahito Nakano <kano@ponko2.jp>
2024-01-06 23:23:44 +0900

    Submit abc025 A

--- abc025/src/bin/a.rs
+++ abc025/src/bin/a.rs
@@ -1,6 +1,20 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+        n: usize,
+    }
+
+    let mut c = 0;
+    for i in &s {
+        for j in &s {
+            c += 1;
+            if c == n {
+                println!("{i}{j}");
+                return;
+            }
+        }
+    }
 }
