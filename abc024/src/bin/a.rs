a2e0546bdcb95db467cd7b7df673a5c0c4087657
Takahito Nakano <kano@ponko2.jp>
2024-01-07 22:55:10 +0900

    Submit abc024 A

--- abc024/src/bin/a.rs
+++ abc024/src/bin/a.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c, k): (usize, usize, usize, usize),
+        (s, t): (usize, usize),
+    }
+
+    println!(
+        "{}",
+        a * s + b * t - {
+            let st = s + t;
+            if st >= k {
+                st * c
+            } else {
+                0
+            }
+        }
+    );
 }
