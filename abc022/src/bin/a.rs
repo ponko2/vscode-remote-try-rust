2892c3d6a8ae7b9babaabed6f6e54590aa4f682e
Takahito Nakano <kano@ponko2.jp>
2024-01-09 20:53:49 +0900

    Submit abc022 A

--- abc022/src/bin/a.rs
+++ abc022/src/bin/a.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, s, t): (usize, i32, i32),
+        mut w: i32,
+        mut a: [i32; n - 1],
+    }
+
+    a.insert(0, 0);
+
+    let mut ans = 0;
+    for a in &a {
+        w += a;
+        if w >= s && w <= t {
+            ans += 1;
+        }
+    }
+
+    println!("{ans}");
 }
