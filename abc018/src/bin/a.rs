55fcee8b656117a6a88210879d18a8d1e016d2e7
Takahito Nakano <kano@ponko2.jp>
2024-01-13 21:19:15 +0900

    Submit abc018 A

--- abc018/src/bin/a.rs
+++ abc018/src/bin/a.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        abc: [usize; 3],
+    }
+
+    let mut s = abc.clone();
+    s.sort();
+    s.reverse();
+
+    for i in abc {
+        println!("{}", s.iter().position(|&v| v == i).unwrap() + 1);
+    }
 }
