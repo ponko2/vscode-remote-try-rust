9876d4e145dabdfcf0e3037485a38f23a3e7624c
Takahito Nakano <kano@ponko2.jp>
2024-01-14 22:39:23 +0900

    Submit abc017 A

--- abc017/src/bin/a.rs
+++ abc017/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        es: [(f64, f64); 3],
+    }
+
+    println!("{}", es.iter().map(|(e, s)| e * s / 10.0).sum::<f64>());
 }
