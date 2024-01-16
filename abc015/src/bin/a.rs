e58264af4734706f292fc4a9c76c26a72a530493
Takahito Nakano <kano@ponko2.jp>
2024-01-16 21:50:29 +0900

    Submit abc015 A

--- abc015/src/bin/a.rs
+++ abc015/src/bin/a.rs
@@ -2,5 +2,10 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: String,
+        b: String,
+    }
+
+    println!("{}", if a.len() > b.len() { a } else { b });
 }
