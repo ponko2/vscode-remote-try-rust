24a752ac2576d5426fff3bc731dadbc46b23898e
Takahito Nakano <kano@ponko2.jp>
2024-01-30 10:35:36 +0900

    Submit abc028 A

--- abc028/src/bin/a.rs
+++ abc028/src/bin/a.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    println!(
+        "{}",
+        if n <= 59 {
+            "Bad"
+        } else if (60..=89).contains(&n) {
+            "Good"
+        } else if (90..=99).contains(&n) {
+            "Great"
+        } else if n == 100 {
+            "Perfect"
+        } else {
+            unreachable!()
+        }
+    )
 }
