634db967d4f1f2469d0ad6a72ff32aa66bd4b4a7
Takahito Nakano <kano@ponko2.jp>
2024-02-01 20:47:15 +0900

    Submit abc030 A

--- abc030/src/bin/a.rs
+++ abc030/src/bin/a.rs
@@ -1,6 +1,19 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering::*;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c, d): (f32, f32, f32, f32),
+    }
+
+    println!(
+        "{}",
+        match (b / a).partial_cmp(&(d / c)) {
+            Some(Greater) => "TAKAHASHI",
+            Some(Equal) => "DRAW",
+            Some(Less) => "AOKI",
+            None => unreachable!(),
+        }
+    );
 }
