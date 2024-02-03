4a0a4c218e62e04f6e6515a358f1ee9f3e85b194
Takahito Nakano <kano@ponko2.jp>
2024-02-03 20:04:00 +0900

    Submit abc032 A

--- abc032/src/bin/a.rs
+++ abc032/src/bin/a.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: usize,
+        b: usize,
+        n: usize,
+    }
+
+    println!("{}", (n..).find(|x| x % a == 0 && x % b == 0).unwrap());
 }
