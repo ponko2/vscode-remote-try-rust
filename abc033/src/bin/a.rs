885813f2cba75c149c3016e9ca1a365aa62bd440
Takahito Nakano <kano@ponko2.jp>
2024-02-04 01:54:14 +0900

    Submit abc033 A

--- abc033/src/bin/a.rs
+++ abc033/src/bin/a.rs
@@ -1,6 +1,16 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: Chars,
+    }
+
+    let (f, r) = n.split_first().unwrap();
+
+    if r.iter().all(|i| i == f) {
+        println!("SAME");
+    } else {
+        println!("DIFFERENT");
+    }
 }
