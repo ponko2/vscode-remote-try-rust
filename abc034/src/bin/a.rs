777583d2ff361a58f76390856eed743904e2a0c3
Takahito Nakano <kano@ponko2.jp>
2024-02-06 20:13:39 +0900

    Submit abc034 A

--- abc034/src/bin/a.rs
+++ abc034/src/bin/a.rs
@@ -1,6 +1,18 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering::*;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (x, y): (u32, u32),
+    }
+
+    println!(
+        "{}",
+        match y.cmp(&x) {
+            Greater => "Better",
+            Less => "Worse",
+            _ => unreachable!(),
+        }
+    )
 }
