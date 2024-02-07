4398dfef2b7ce9c1bc7d1884951a4beed7030462
Takahito Nakano <kano@ponko2.jp>
2024-02-07 20:36:22 +0900

    Submit abc035 A

--- abc035/src/bin/a.rs
+++ abc035/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (w, h): (usize, usize),
+    }
+
+    println!("{}", if h * 4 == w * 3 { "4:3" } else { "16:9" })
 }
