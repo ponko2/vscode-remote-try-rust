84944cc791d24d32862e81daed51a1617ddf0267
Takahito Nakano <kano@ponko2.jp>
2024-02-13 19:55:30 +0900

    Submit abc039 A

--- abc039/src/bin/a.rs
+++ abc039/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c): (usize, usize, usize),
+    }
+
+    println!("{}", (a * b + a * c + b * c) * 2);
 }
