a00a45b0ee8364a6b068f7a2b02327195faef77e
Takahito Nakano <kano@ponko2.jp>
2024-03-15 19:53:19 +0900

    Submit abc046 A

--- abc046/src/bin/a.rs
+++ abc046/src/bin/a.rs
@@ -1,6 +1,11 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        abc: [usize; 3],
+    }
+
+    println!("{}", abc.iter().unique().count());
 }
