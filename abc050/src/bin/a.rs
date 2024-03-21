d96b91083a54d64b0e1388ba8abdbb6fe016d985
Takahito Nakano <kano@ponko2.jp>
2024-03-21 20:04:59 +0900

    Submit abc050 A

--- abc050/src/bin/a.rs
+++ abc050/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, op, b): (isize, char, isize),
+    }
+
+    println!(
+        "{}",
+        match op {
+            '+' => a + b,
+            '-' => a - b,
+            _ => unreachable!(),
+        }
+    )
 }
