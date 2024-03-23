42f93688ccff4f2cb956df029669b1db17441631
Takahito Nakano <kano@ponko2.jp>
2024-03-24 05:45:55 +0900

    Submit tessoku-book B08

--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -2,5 +2,32 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        xy: [(usize, usize); n],
+        q: usize,
+        abcd: [(usize, usize, usize, usize); q],
+    }
+
+    let mut z = vec![vec![0; 1501]; 1501];
+
+    for (x, y) in xy {
+        z[x][y] += 1;
+    }
+
+    for zi in z.iter_mut().skip(1).take(1500) {
+        for j in 1..=1500 {
+            zi[j] += zi[j - 1];
+        }
+    }
+
+    for j in 1..=1500 {
+        for i in 1..=1500 {
+            z[i][j] += z[i - 1][j];
+        }
+    }
+
+    for (a, b, c, d) in abcd {
+        println!("{}", z[c][d] + z[a - 1][b - 1] - z[a - 1][d] - z[c][b - 1]);
+    }
 }
