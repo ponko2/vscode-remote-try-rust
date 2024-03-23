e07161c584c3f1b8b874f9a630d0fcd618b01505
Takahito Nakano <kano@ponko2.jp>
2024-03-24 06:18:48 +0900

    Submit tessoku-book A09

--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -1,6 +1,35 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h, w, n): (usize, usize, usize),
+        abcd: [(usize, usize, usize, usize); n],
+    }
+
+    let mut x = vec![vec![0; w + 2]; h + 2];
+
+    for (a, b, c, d) in abcd {
+        x[a][b] += 1;
+        x[a][d + 1] -= 1;
+        x[c + 1][b] -= 1;
+        x[c + 1][d + 1] += 1;
+    }
+
+    for xi in x.iter_mut().skip(1).take(h) {
+        for j in 1..=w {
+            xi[j] += xi[j - 1];
+        }
+    }
+
+    for j in 1..=w {
+        for i in 1..=h {
+            x[i][j] += x[i - 1][j];
+        }
+    }
+
+    for xi in x.iter().skip(1).take(h) {
+        println!("{}", xi.iter().skip(1).take(w).join(" "));
+    }
 }
