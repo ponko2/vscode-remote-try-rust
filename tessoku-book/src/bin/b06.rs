d476d075de96a2538c82ad1107d8b801a0ed0691
Takahito Nakano <kano@ponko2.jp>
2024-03-24 03:44:31 +0900

    Submit tessoku-book B06

--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -1,6 +1,30 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+        q: usize,
+        lr: [(usize, usize); q],
+    }
+
+    let b: Vec<_> = std::iter::once(0)
+        .chain(a.iter().scan(0, |state, &a| {
+            *state += if a == 0 { -1 } else { 1 };
+            Some(*state)
+        }))
+        .collect();
+
+    for (l, r) in lr {
+        println!(
+            "{}",
+            match b[r].cmp(&b[l - 1]) {
+                Ordering::Greater => "win",
+                Ordering::Less => "lose",
+                Ordering::Equal => "draw",
+            }
+        )
+    }
 }
