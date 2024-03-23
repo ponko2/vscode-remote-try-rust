2ca0a24878f6cabb6b0c43d021ccb7eb478c2834
Takahito Nakano <kano@ponko2.jp>
2024-03-24 04:14:32 +0900

    Submit tessoku-book B07

--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -2,5 +2,22 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        t: usize,
+        n: usize,
+        lr: [(usize, usize); n],
+    }
+
+    let mut b = vec![0; t + 1];
+    for (l, r) in lr {
+        b[l] += 1;
+        b[r] -= 1;
+    }
+
+    for ans in b.iter().take(t).scan(0, |state, &x| {
+        *state += x;
+        Some(*state)
+    }) {
+        println!("{ans}");
+    }
 }
