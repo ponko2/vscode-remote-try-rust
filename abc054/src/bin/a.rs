47e52f6ca89d7e4b299784f4731a007b44922ce2
Takahito Nakano <kano@ponko2.jp>
2024-03-26 20:57:21 +0900

    Submit abc054 A

--- abc054/src/bin/a.rs
+++ abc054/src/bin/a.rs
@@ -1,6 +1,20 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    let c = |x| if x == 1 { 14 } else { x };
+
+    println!(
+        "{}",
+        match c(a).cmp(&c(b)) {
+            Ordering::Greater => "Alice",
+            Ordering::Less => "Bob",
+            Ordering::Equal => "Draw",
+        }
+    );
 }
