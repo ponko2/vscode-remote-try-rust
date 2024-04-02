a8bfa6e45c432166e389d21f08143db074c88098
Takahito Nakano <kano@ponko2.jp>
2024-04-02 20:19:38 +0900

    Submit abc060 A

--- abc060/src/bin/a.rs
+++ abc060/src/bin/a.rs
@@ -1,6 +1,22 @@
-use proconio::{fastout, input};
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        abc: [Chars; 3],
+    }
+
+    println!(
+        "{}",
+        if abc
+            .iter()
+            .tuple_windows()
+            .all(|(l, r)| l.last().unwrap() == r.first().unwrap())
+        {
+            "YES"
+        } else {
+            "NO"
+        }
+    );
 }
