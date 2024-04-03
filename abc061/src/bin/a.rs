71dcc8c5d199a6f3831a9e7cbdfdf848cac25ba7
Takahito Nakano <kano@ponko2.jp>
2024-04-03 19:42:42 +0900

    Submit abc061 A

--- abc061/src/bin/a.rs
+++ abc061/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c): (i32, i32, i32),
+    }
+
+    println!("{}", if c >= a && c <= b { "Yes" } else { "No" });
 }
