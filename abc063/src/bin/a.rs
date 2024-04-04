282605ac9fe17dbcdfea99ddc086861dc406d68c
Takahito Nakano <kano@ponko2.jp>
2024-04-04 20:22:45 +0900

    Submit abc063 A

--- abc063/src/bin/a.rs
+++ abc063/src/bin/a.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    let ans = a + b;
+
+    if ans < 10 {
+        println!("{ans}");
+    } else {
+        println!("error");
+    }
 }
