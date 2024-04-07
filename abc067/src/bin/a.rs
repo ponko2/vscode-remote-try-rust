fa4ffacfad613950077e7abad77d2d550cdf69e3
Takahito Nakano <kano@ponko2.jp>
2024-04-07 20:12:53 +0900

    Submit abc067 A

--- abc067/src/bin/a.rs
+++ abc067/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!(
+        "{}",
+        if a % 3 == 0 || b % 3 == 0 || (a + b) % 3 == 0 {
+            "Possible"
+        } else {
+            "Impossible"
+        }
+    )
 }
