fd5c5854f472a94277903801e50e2899de7d730a
Takahito Nakano <kano@ponko2.jp>
2024-04-19 19:34:16 +0900

    Submit abc079 A

--- abc079/src/bin/a.rs
+++ abc079/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: Chars,
+    }
+
+    println!(
+        "{}",
+        if n[1] == n[2] && (n[0] == n[1] || n[2] == n[3]) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
