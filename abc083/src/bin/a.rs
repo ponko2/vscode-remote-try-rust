789af48dfa61dc49bd46838fe46f131b929d0645
Takahito Nakano <kano@ponko2.jp>
2024-04-23 19:42:24 +0900

    Submit abc083 A

--- abc083/src/bin/a.rs
+++ abc083/src/bin/a.rs
@@ -1,6 +1,18 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c, d): (usize, usize, usize, usize),
+    }
+
+    println!(
+        "{}",
+        match (a + b).cmp(&(c + d)) {
+            Ordering::Greater => "Left",
+            Ordering::Equal => "Balanced",
+            Ordering::Less => "Right",
+        }
+    );
 }
