8424922cae1a555e5f99e2fd2638d2ded0bd12bd
Takahito Nakano <kano@ponko2.jp>
2024-04-27 03:38:18 +0900

    Submit abc088 A

--- abc088/src/bin/a.rs
+++ abc088/src/bin/a.rs
@@ -2,5 +2,10 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: usize,
+    }
+
+    println!("{}", if n % 500 <= a { "Yes" } else { "No" });
 }
