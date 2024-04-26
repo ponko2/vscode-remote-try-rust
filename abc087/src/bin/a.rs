10f304245830b87d29919a87af674e302f425067
Takahito Nakano <kano@ponko2.jp>
2024-04-26 22:07:57 +0900

    Submit abc087 A

--- abc087/src/bin/a.rs
+++ abc087/src/bin/a.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        x: usize,
+        a: usize,
+        b: usize,
+    }
+
+    println!("{}", (x - a) % b);
 }
