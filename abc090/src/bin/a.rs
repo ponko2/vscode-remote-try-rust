538167ea9c5aea77a73a480b39cc8e824244d75c
Takahito Nakano <kano@ponko2.jp>
2024-04-29 17:31:31 +0900

    Submit abc090 A

--- abc090/src/bin/a.rs
+++ abc090/src/bin/a.rs
@@ -1,6 +1,10 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        c: [Chars; 3],
+    }
+
+    println!("{}{}{}", c[0][0], c[1][1], c[2][2]);
 }
