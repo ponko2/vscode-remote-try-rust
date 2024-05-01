4798afc8dfa10ad1a816251f2855c210ba464449
Takahito Nakano <kano@ponko2.jp>
2024-05-01 20:29:52 +0900

    Submit abc092 A

--- abc092/src/bin/a.rs
+++ abc092/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+    }
+
+    println!("{}", a.min(b) + c.min(d));
 }
