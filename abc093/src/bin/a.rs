473ebcd8316fc3de9352cf5d6547d852896e759c
Takahito Nakano <kano@ponko2.jp>
2024-05-02 15:00:36 +0900

    Submit abc093 A

--- abc093/src/bin/a.rs
+++ abc093/src/bin/a.rs
@@ -1,6 +1,18 @@
-use proconio::{fastout, input};
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    println!(
+        "{}",
+        if s.iter().sorted().collect::<String>() == "abc" {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
