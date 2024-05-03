96776e16cab7aad3f8e7be1e85b9912da17ba7d4
Takahito Nakano <kano@ponko2.jp>
2024-05-03 23:46:38 +0900

    Submit abc094 A

--- abc094/src/bin/a.rs
+++ abc094/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, x): (usize, usize, usize),
+    }
+
+    println!("{}", if x >= a && x - a <= b { "YES" } else { "NO" });
 }
