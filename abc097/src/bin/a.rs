334f5fdf1325117d1faa40ed38812e4ccd188e2a
Takahito Nakano <kano@ponko2.jp>
2024-05-07 20:18:02 +0900

    Submit abc097 A

--- abc097/src/bin/a.rs
+++ abc097/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c, d): (usize, usize, usize, usize),
+    }
+
+    println!(
+        "{}",
+        if a.abs_diff(c) <= d || a.abs_diff(b).max(b.abs_diff(c)) <= d {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
