30b5f1d7e9f87e49613321a37e28837893a8a982
Takahito Nakano <kano@ponko2.jp>
2024-05-10 00:23:04 +0900

    Submit abc100 A

--- abc100/src/bin/a.rs
+++ abc100/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!("{}", if a <= 8 && b <= 8 { "Yay!" } else { ":(" });
 }
