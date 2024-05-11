919dc78246b49d2ed7dfcc20f40cd3e30bd32e35
Takahito Nakano <kano@ponko2.jp>
2024-05-11 22:04:35 +0900

    Submit abc101 A

--- abc101/src/bin/a.rs
+++ abc101/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars
+    }
+
+    println!(
+        "{}",
+        s.iter().fold(0, |acc, c| match c {
+            '+' => acc + 1,
+            '-' => acc - 1,
+            _ => unreachable!(),
+        })
+    )
 }
