cb55c25453d1da3da0ce46a8e847e21940c39872
Takahito Nakano <kano@ponko2.jp>
2024-05-14 20:15:21 +0900

    Submit abc105 A

--- abc105/src/bin/a.rs
+++ abc105/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, k): (usize, usize),
+    }
+
+    println!("{}", if n % k == 0 { 0 } else { 1 });
 }
