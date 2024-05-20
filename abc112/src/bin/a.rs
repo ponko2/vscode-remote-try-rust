e79e641bbc295a420a4d65994309302aa8d58a6d
Takahito Nakano <kano@ponko2.jp>
2024-05-20 19:45:56 +0900

    Submit abc112 A

--- abc112/src/bin/a.rs
+++ abc112/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    if n == 1 {
+        println!("Hello World");
+    } else {
+        input! {
+            a: usize,
+            b: usize,
+        }
+        println!("{}", a + b);
+    }
 }
