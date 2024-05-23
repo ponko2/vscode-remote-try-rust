f794841b43a454e46abc021bc7939d59050056f7
Takahito Nakano <kano@ponko2.jp>
2024-05-23 15:04:25 +0900

    Submit abc115 A

--- abc115/src/bin/a.rs
+++ abc115/src/bin/a.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        d: usize,
+    }
+
+    println!(
+        "{}",
+        match d {
+            25 => "Christmas",
+            24 => "Christmas Eve",
+            23 => "Christmas Eve Eve",
+            22 => "Christmas Eve Eve Eve",
+            _ => unreachable!(),
+        }
+    )
 }
