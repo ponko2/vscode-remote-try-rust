d47c35b88a0379d23030c5ecec806411dd651785
Takahito Nakano <kano@ponko2.jp>
2024-05-30 21:08:03 +0900

    Submit abc119 A

--- abc119/src/bin/a.rs
+++ abc119/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    println!(
+        "{}",
+        if s.replace('/', "").parse::<i32>().unwrap() <= 20190430 {
+            "Heisei"
+        } else {
+            "TBD"
+        }
+    );
 }
