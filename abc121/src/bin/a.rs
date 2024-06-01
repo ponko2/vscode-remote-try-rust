8bac842386154a6c711174b9fb825d2026096d48
Takahito Nakano <kano@ponko2.jp>
2024-06-01 14:24:00 +0900

    Submit abc121 A

--- abc121/src/bin/a.rs
+++ abc121/src/bin/a.rs
@@ -2,5 +2,10 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h1, w1): (usize, usize),
+        (h2, w2): (usize, usize),
+    }
+
+    println!("{}", (h1 - h2) * (w1 - w2));
 }
