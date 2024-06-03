76ee323b3cf4567e4021336439a747412555ce30
Takahito Nakano <kano@ponko2.jp>
2024-06-03 19:58:00 +0900

    Submit abc123 A

--- abc123/src/bin/a.rs
+++ abc123/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, _b, _c, _d, e, k): (usize, usize, usize, usize, usize, usize),
+    }
+
+    println!("{}", if e - a > k { ":(" } else { "Yay!" });
 }
