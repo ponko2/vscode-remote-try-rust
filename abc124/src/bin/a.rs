1a0cd6f2a5c8a552970369065ef9d9c9dd0ffcf3
Takahito Nakano <kano@ponko2.jp>
2024-06-04 20:15:28 +0900

    Submit abc124 A

--- abc124/src/bin/a.rs
+++ abc124/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!("{}", (a + b).max(a.max(b) * 2 - 1));
 }
