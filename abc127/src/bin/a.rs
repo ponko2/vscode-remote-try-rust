d7322c59f6f38ad10308391f0ae70ba2cfde8bba
Takahito Nakano <kano@ponko2.jp>
2024-06-07 13:02:12 +0000

    Submit abc127 A

--- abc127/src/bin/a.rs
+++ abc127/src/bin/a.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!(
+        "{}",
+        if a >= 13 {
+            b
+        } else if (6..=12).contains(&a) {
+            b / 2
+        } else {
+            0
+        }
+    )
 }
