6e2252e3c307a19d8c254109bccfb2ba032124c1
Takahito Nakano <kano@ponko2.jp>
2024-06-09 00:34:12 +0900

    Submit abc129 A

--- abc129/src/bin/a.rs
+++ abc129/src/bin/a.rs
@@ -1,6 +1,11 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        pqr: [usize; 3],
+    }
+
+    println!("{}", pqr.iter().sorted().take(2).sum::<usize>());
 }
