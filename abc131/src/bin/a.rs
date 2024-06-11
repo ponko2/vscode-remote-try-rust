070b87b548cccd84ed0a7f1facb91417ad2d3b01
Takahito Nakano <kano@ponko2.jp>
2024-06-11 20:24:22 +0900

    Submit abc131 A

--- abc131/src/bin/a.rs
+++ abc131/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    for i in 1..s.len() {
+        if s[i - 1] == s[i] {
+            println!("Bad");
+            return;
+        }
+    }
+
+    println!("Good");
 }
