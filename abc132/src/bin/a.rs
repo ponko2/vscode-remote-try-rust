dfc6268381f1506173a86530d579ab77dce316fc
Takahito Nakano <kano@ponko2.jp>
2024-06-12 20:46:11 +0900

    Submit abc132 A

--- abc132/src/bin/a.rs
+++ abc132/src/bin/a.rs
@@ -1,6 +1,19 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        mut s: Chars,
+    }
+
+    s.sort();
+
+    println!(
+        "{}",
+        if s[0] == s[1] && s[1] != s[2] && s[2] == s[3] {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
 }
