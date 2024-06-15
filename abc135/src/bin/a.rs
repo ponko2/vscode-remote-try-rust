acd196180554f9a29977364183cc2ddfd3bb8daf
Takahito Nakano <kano@ponko2.jp>
2024-06-15 18:34:00 +0900

    Submit abc135 A

--- abc135/src/bin/a.rs
+++ abc135/src/bin/a.rs
@@ -2,5 +2,13 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    if (a + b) % 2 == 0 {
+        println!("{}", (a + b) / 2);
+    } else {
+        println!("IMPOSSIBLE");
+    }
 }
