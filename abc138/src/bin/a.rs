fe9c32ea1d74c3b616e5da9705bb0d167a25e3c5
Takahito Nakano <kano@ponko2.jp>
2024-06-19 20:18:16 +0900

    Submit abc138 A

--- abc138/src/bin/a.rs
+++ abc138/src/bin/a.rs
@@ -2,5 +2,14 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: usize,
+        s: String,
+    }
+
+    if a >= 3200 {
+        println!("{s}");
+    } else {
+        println!("red");
+    }
 }
