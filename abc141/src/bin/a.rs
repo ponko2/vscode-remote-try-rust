c9ccf76861dc7aa9274bd33fc776be7260cbea5a
Takahito Nakano <kano@ponko2.jp>
2024-06-22 22:50:43 +0900

    Submit abc141 A

--- abc141/src/bin/a.rs
+++ abc141/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    println!(
+        "{}",
+        match s.as_str() {
+            "Sunny" => "Cloudy",
+            "Cloudy" => "Rainy",
+            "Rainy" => "Sunny",
+            _ => unreachable!(),
+        }
+    );
 }
