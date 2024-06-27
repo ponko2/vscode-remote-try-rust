6971f740b79f9988f737ed8e962432c145603547
Takahito Nakano <kano@ponko2.jp>
2024-06-27 20:06:22 +0900

    Submit abc144 A

--- abc144/src/bin/a.rs
+++ abc144/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (i32, i32),
+    }
+
+    println!("{}", if a.max(b) <= 9 { a * b } else { -1 });
 }
