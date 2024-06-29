480f2ddd6923cd81a8c9e831f60153a990618177
Takahito Nakano <kano@ponko2.jp>
2024-06-30 00:01:38 +0900

    Submit abc146 A

--- abc146/src/bin/a.rs
+++ abc146/src/bin/a.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    println!(
+        "{}",
+        match s.as_str() {
+            "SUN" => 7,
+            "MON" => 6,
+            "TUE" => 5,
+            "WED" => 4,
+            "THU" => 3,
+            "FRI" => 2,
+            "SAT" => 1,
+            _ => unreachable!(),
+        }
+    );
 }
