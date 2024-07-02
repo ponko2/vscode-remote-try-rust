675de1333fec4089ac72ca0cd0edc0a246f868d8
Takahito Nakano <kano@ponko2.jp>
2024-07-02 19:29:31 +0900

    Submit abc150 A

--- abc150/src/bin/a.rs
+++ abc150/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (k, x): (usize, usize),
+    }
+
+    println!("{}", if 500 * k >= x { "Yes" } else { "No" })
 }
