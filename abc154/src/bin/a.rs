617354dcf2dd01b434e11217712269e7f20faaaa
Takahito Nakano <kano@ponko2.jp>
2024-07-05 20:35:31 +0900

    Submit abc154 A

--- abc154/src/bin/a.rs
+++ abc154/src/bin/a.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (s, t): (String, String),
+        (mut a, mut b): (usize, usize),
+        u: String,
+    }
+
+    if u == s {
+        a -= 1;
+    }
+
+    if u == t {
+        b -= 1;
+    }
+
+    println!("{a} {b}")
 }
