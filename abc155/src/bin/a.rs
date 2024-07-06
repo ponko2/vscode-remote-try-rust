0ddfe02d95d235824fd09a7b382f9fdcbf73fe60
Takahito Nakano <kano@ponko2.jp>
2024-07-06 23:55:57 +0900

    Submit abc155 A

--- abc155/src/bin/a.rs
+++ abc155/src/bin/a.rs
@@ -1,6 +1,18 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        abc: [usize; 3],
+    }
+
+    println!(
+        "{}",
+        if abc.iter().unique().count() == 2 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
