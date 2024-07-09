feac112167a0df5f9af15b3fd8eb1bdd4f13fc08
Takahito Nakano <kano@ponko2.jp>
2024-07-09 20:12:03 +0900

    Submit abc158 A

--- abc158/src/bin/a.rs
+++ abc158/src/bin/a.rs
@@ -1,6 +1,18 @@
-use proconio::{fastout, input};
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    println!(
+        "{}",
+        if s.iter().unique().count() == 2 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
