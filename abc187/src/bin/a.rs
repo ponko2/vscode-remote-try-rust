91e4b4679a2d72e7ddc56a779f940172a0c6d6ad
Takahito Nakano <kano@ponko2.jp>
2024-07-16 20:38:55 +0900

    Submit abc187 A

--- abc187/src/bin/a.rs
+++ abc187/src/bin/a.rs
@@ -1,6 +1,16 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (Chars, Chars),
+    }
+
+    println!(
+        "{}",
+        a.iter()
+            .map(|&c| c.to_digit(10).unwrap())
+            .sum::<u32>()
+            .max(b.iter().map(|&c| c.to_digit(10).unwrap()).sum::<u32>())
+    );
 }
