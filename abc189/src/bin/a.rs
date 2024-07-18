716813730366731b8818fbe75cc26c7ccc4e2e84
Takahito Nakano <kano@ponko2.jp>
2024-07-18 19:50:27 +0900

    Submit abc189 A

--- abc189/src/bin/a.rs
+++ abc189/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        c: Chars,
+    }
+
+    println!(
+        "{}",
+        if c[0] == c[1] && c[1] == c[2] {
+            "Won"
+        } else {
+            "Lost"
+        }
+    );
 }
