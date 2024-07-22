c326554a3b1523c98ec95ee6e10452790b9093cd
Takahito Nakano <kano@ponko2.jp>
2024-07-22 20:12:39 +0900

    Submit abc194 A

--- abc194/src/bin/a.rs
+++ abc194/src/bin/a.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!(
+        "{}",
+        if a + b >= 15 && b >= 8 {
+            1
+        } else if a + b >= 10 && b >= 3 {
+            2
+        } else if a + b >= 3 {
+            3
+        } else {
+            4
+        }
+    );
 }
