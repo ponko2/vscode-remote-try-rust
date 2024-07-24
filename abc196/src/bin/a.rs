f6e3480993b1ec1255ed3060ac269f3045cc4b89
Takahito Nakano <kano@ponko2.jp>
2024-07-24 20:11:39 +0900

    Submit abc196 A

--- abc196/src/bin/a.rs
+++ abc196/src/bin/a.rs
@@ -2,5 +2,10 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (i32, i32),
+        (c, d): (i32, i32),
+    }
+
+    println!("{}", a.max(b) - c.min(d));
 }
