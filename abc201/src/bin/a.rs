e9f2b6aabb3b048e55b01b96005ff6b203ec0c12
Takahito Nakano <kano@ponko2.jp>
2024-07-30 23:48:27 +0900

    Submit abc201 A

--- abc201/src/bin/a.rs
+++ abc201/src/bin/a.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        mut a: [usize; 3],
+    }
+
+    a.sort();
+
+    println!(
+        "{}",
+        if a[2] - a[1] == a[1] - a[0] {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
