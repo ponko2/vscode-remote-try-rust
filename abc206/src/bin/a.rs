2a1b8200dfa8633110dbb8f1e14f4f0b290a8f92
Takahito Nakano <kano@ponko2.jp>
2024-08-03 23:54:15 +0900

    Submit abc206 A

--- abc206/src/bin/a.rs
+++ abc206/src/bin/a.rs
@@ -1,6 +1,19 @@
+use std::cmp::Ordering;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: f32,
+    }
+
+    println!(
+        "{}",
+        match ((n * 1.08) as i32).cmp(&206) {
+            Ordering::Less => "Yay!",
+            Ordering::Equal => "so-so",
+            Ordering::Greater => ":(",
+        }
+    );
 }
