afafe2ca6aa6da61db12d93f51dff6f6db9a9013
Takahito Nakano <kano@ponko2.jp>
2024-08-05 19:51:10 +0900

    Submit abc208 A

--- abc208/src/bin/a.rs
+++ abc208/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!("{}", if a <= b && a * 6 >= b { "Yes" } else { "No" });
 }
