ed6de3abbf7812a35ec5a04f9619c71ab85294b8
Takahito Nakano <kano@ponko2.jp>
2024-08-20 18:44:14 +0900

    Submit abc218 A

--- contests/abc218/src/bin/a.rs
+++ contests/abc218/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: String,
+    }
+
+    println!(
+        "{}",
+        match s.chars().nth(n - 1).unwrap() {
+            'o' => "Yes",
+            'x' => "No",
+            _ => unreachable!(),
+        }
+    );
 }
