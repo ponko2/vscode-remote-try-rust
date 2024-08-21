e3df75c94559d33ac04365831f81facbfbab4703
Takahito Nakano <kano@ponko2.jp>
2024-08-21 20:10:35 +0900

    Submit abc219 A

--- contests/abc219/src/bin/a.rs
+++ contests/abc219/src/bin/a.rs
@@ -2,5 +2,15 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        x: usize,
+    }
+
+    match x {
+        0..=39 => println!("{}", 40 - x),
+        40..=69 => println!("{}", 70 - x),
+        70..=89 => println!("{}", 90 - x),
+        90.. => println!("expert"),
+        _ => unreachable!(),
+    }
 }
