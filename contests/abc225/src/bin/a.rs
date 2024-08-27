5ce38fcec385c7411de4c2dc6b5df36e1a797c95
Takahito Nakano <kano@ponko2.jp>
2024-08-27 21:59:03 +0900

    Submit abc225 A

--- contests/abc225/src/bin/a.rs
+++ contests/abc225/src/bin/a.rs
@@ -1,6 +1,11 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    println!("{}", s.chars().permutations(3).unique().count());
 }
