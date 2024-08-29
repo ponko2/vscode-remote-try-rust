370f1d34c70ca7a0d3c832e9b9295e3c703989d6
Takahito Nakano <kano@ponko2.jp>
2024-08-29 20:10:39 +0900

    Submit abc228 A

--- contests/abc228/src/bin/a.rs
+++ contests/abc228/src/bin/a.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (s, t, x): (usize, usize, usize),
+    }
+
+    println!(
+        "{}",
+        if if s < t {
+            s <= x && t > x
+        } else {
+            t > x || s <= x
+        } {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
