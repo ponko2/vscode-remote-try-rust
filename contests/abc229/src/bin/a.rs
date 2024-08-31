f8e0fca8cfd348594b732a59e6f0a47f9e3460aa
Takahito Nakano <kano@ponko2.jp>
2024-08-31 18:51:39 +0900

    Submit abc229 A

--- contests/abc229/src/bin/a.rs
+++ contests/abc229/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: [String; 2],
+    }
+
+    println!(
+        "{}",
+        if s != [".#", "#."] && s != ["#.", ".#"] {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
