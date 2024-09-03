642562ac14b35cc632376dcff9ad8b2cfd127354
Takahito Nakano <kano@ponko2.jp>
2024-09-03 21:33:15 +0900

    Submit abc232 A

--- contests/abc232/src/bin/a.rs
+++ contests/abc232/src/bin/a.rs
@@ -1,6 +1,13 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    println!(
+        "{}",
+        s[0].to_digit(10).unwrap() * s[2].to_digit(10).unwrap()
+    );
 }
