cc294884e147ba95fbdf4b7679e9d09d8ca08506
Takahito Nakano <kano@ponko2.jp>
2024-09-05 22:47:47 +0900

    Submit abc234 A

--- contests/abc234/src/bin/a.rs
+++ contests/abc234/src/bin/a.rs
@@ -1,6 +1,14 @@
 use proconio::{fastout, input};
 
+fn f(x: usize) -> usize {
+    x.pow(2) + 2 * x + 3
+}
+
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        t: usize,
+    }
+
+    println!("{}", f(f(f(t) + t) + f(f(t))));
 }
