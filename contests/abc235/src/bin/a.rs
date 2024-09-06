852c274e567546b6ed769e43e8e90001241bed54
Takahito Nakano <kano@ponko2.jp>
2024-09-06 20:57:57 +0900

    Submit abc235 A

--- contests/abc235/src/bin/a.rs
+++ contests/abc235/src/bin/a.rs
@@ -1,6 +1,20 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        abc: Chars,
+    }
+
+    println!(
+        "{}",
+        vec![
+            format!("{}{}{}", abc[0], abc[1], abc[2]),
+            format!("{}{}{}", abc[1], abc[2], abc[0]),
+            format!("{}{}{}", abc[2], abc[0], abc[1])
+        ]
+        .iter()
+        .map(|x| x.parse::<usize>().unwrap())
+        .sum::<usize>()
+    );
 }
