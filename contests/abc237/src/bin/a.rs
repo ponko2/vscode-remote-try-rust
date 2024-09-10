749beb01b8dac5370ba25c2f2516f73d863b4ccf
Takahito Nakano <kano@ponko2.jp>
2024-09-10 19:48:43 +0900

    Submit abc237 A

--- contests/abc237/src/bin/a.rs
+++ contests/abc237/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i64,
+    }
+
+    println!(
+        "{}",
+        if (-2_i64).pow(31) <= n && 2_i64.pow(31) > n {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
