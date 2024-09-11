1bc6c1844636d05c32ae47b3b80eb5ca28523de0
Takahito Nakano <kano@ponko2.jp>
2024-09-11 19:20:13 +0900

    Submit abc238 A

--- contests/abc238/src/bin/a.rs
+++ contests/abc238/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: u32,
+    }
+
+    println!("{}", if (2..=4).contains(&n) { "No" } else { "Yes" });
 }
