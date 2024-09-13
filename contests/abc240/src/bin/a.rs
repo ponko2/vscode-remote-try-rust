cb6ef09178ec3ac899681dfe34c3bd1d26dce489
Takahito Nakano <kano@ponko2.jp>
2024-09-13 19:49:22 +0900

    Submit abc240 A

--- contests/abc240/src/bin/a.rs
+++ contests/abc240/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b): (usize, usize),
+    }
+
+    println!(
+        "{}",
+        if [1, 9].contains(&a.abs_diff(b)) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
