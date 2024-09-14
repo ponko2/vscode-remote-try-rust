7663d2594905f70d7f10eab8c905374b83f76f0a
Takahito Nakano <kano@ponko2.jp>
2024-09-15 06:08:43 +0900

    Submit abc242 A

--- contests/abc242/src/bin/a.rs
+++ contests/abc242/src/bin/a.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c, x): (usize, usize, usize, usize),
+    }
+
+    println!(
+        "{:.12}",
+        if a >= x {
+            1.0
+        } else if a < x && b >= x {
+            c as f64 / (b - a) as f64
+        } else {
+            0.0
+        }
+    );
 }
