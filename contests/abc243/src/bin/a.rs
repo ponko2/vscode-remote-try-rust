68e9f9651d91cacc922985fff7f962e8892aeb98
Takahito Nakano <kano@ponko2.jp>
2024-09-16 04:00:24 +0900

    Submit abc243 A

--- contests/abc243/src/bin/a.rs
+++ contests/abc243/src/bin/a.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (mut v, a, b, c): (i32, i32, i32, i32),
+    }
+
+    println!(
+        "{}",
+        loop {
+            v -= a;
+            if v < 0 {
+                break "F";
+            }
+            v -= b;
+            if v < 0 {
+                break "M";
+            }
+            v -= c;
+            if v < 0 {
+                break "T";
+            }
+        }
+    );
 }
