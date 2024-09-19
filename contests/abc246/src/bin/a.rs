c2344f785be6b07b68a8e53d41d7bbf0414c7e43
Takahito Nakano <kano@ponko2.jp>
2024-09-19 19:53:07 +0900

    Submit abc246 A

--- contests/abc246/src/bin/a.rs
+++ contests/abc246/src/bin/a.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        xy: [(i32, i32); 3],
+    }
+
+    let (x, y) = xy.iter().fold((0, 0), |a, b| (a.0 ^ b.0, a.1 ^ b.1));
+
+    println!("{x} {y}");
 }
