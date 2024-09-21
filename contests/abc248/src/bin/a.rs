d990ca14ebca11dbdfea27db68b25e01e7a35004
Takahito Nakano <kano@ponko2.jp>
2024-09-21 20:46:52 +0900

    Submit abc248 A

--- contests/abc248/src/bin/a.rs
+++ contests/abc248/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: String,
+    }
+
+    println!("{}", ('0'..='9').find(|&c| !s.contains(c)).unwrap());
 }
