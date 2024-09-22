319a55f475de54876bc30a96c1776fd47902df77
Takahito Nakano <kano@ponko2.jp>
2024-09-22 19:56:38 +0900

    Submit abc249 A

--- contests/abc249/src/bin/a.rs
+++ contests/abc249/src/bin/a.rs
@@ -1,6 +1,20 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c, d, e, f, x): (usize, usize, usize, usize, usize, usize, usize),
+    }
+
+    println!(
+        "{}",
+        match ((x / (a + c) * a + (x % (a + c)).min(a)) * b)
+            .cmp(&((x / (d + f) * d + (x % (d + f)).min(d)) * e))
+        {
+            Ordering::Greater => "Takahashi",
+            Ordering::Less => "Aoki",
+            Ordering::Equal => "Draw",
+        }
+    );
 }
