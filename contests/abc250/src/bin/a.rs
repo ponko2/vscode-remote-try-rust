a9b8b8fb3df54857d914ae4c780a8620d4ca896f
Takahito Nakano <kano@ponko2.jp>
2024-09-24 21:23:25 +0900

    Submit abc250 A

--- contests/abc250/src/bin/a.rs
+++ contests/abc250/src/bin/a.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h, w): (usize, usize),
+        (r, c): (usize, usize),
+    }
+
+    let mut ans = 0;
+
+    if 1 < r {
+        ans += 1;
+    }
+    if r < h {
+        ans += 1;
+    }
+    if 1 < c {
+        ans += 1;
+    }
+    if c < w {
+        ans += 1;
+    }
+
+    println!("{ans}");
 }
