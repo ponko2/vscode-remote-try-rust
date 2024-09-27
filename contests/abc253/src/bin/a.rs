a506033d06690a5b73625e4b6b44e6a97aa0757c
Takahito Nakano <kano@ponko2.jp>
2024-09-27 20:01:12 +0900

    Submit abc253 A

--- contests/abc253/src/bin/a.rs
+++ contests/abc253/src/bin/a.rs
@@ -1,6 +1,18 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c): (usize, usize, usize),
+    }
+
+    println!(
+        "{}",
+        if b == *[a, b, c].iter().sorted().nth(1).unwrap() {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
