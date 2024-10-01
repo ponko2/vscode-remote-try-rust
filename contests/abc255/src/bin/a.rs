d31151978b7a09580dc870564b12abcf61fdd84b
Takahito Nakano <kano@ponko2.jp>
2024-10-01 21:57:45 +0900

    Submit abc255 A

--- contests/abc255/src/bin/a.rs
+++ contests/abc255/src/bin/a.rs
@@ -1,6 +1,11 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (r, c): (Usize1, Usize1),
+        a: [[usize; 2]; 2],
+    }
+
+    println!("{}", a[r][c]);
 }
